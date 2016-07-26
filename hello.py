3aba15b3bffde72432db05d01cd538501cf9a0e8
taras <taraslegend@mail.com>
2016-07-26 17:28:02 +0300

    added comments

--- hello.py
+++ hello.py
@@ -4,10 +4,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# hello world in 127.0.0.1:5000
 @app.route('/')
 def index():
     return '<h1>Hello world</h1>'
 
-
+# starts app
 if __name__ == '__main__':
     app.run(debug=True)
