6b27704d886ef29c596aaa670ee0d45d37cba75e
taras <taraslegend@mail.com>
2016-07-27 10:26:52 +0300

    add bootstrap to view

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
-
+bootstrap = Bootstrap(app)
 
 # hello world in 127.0.0.1:5000
 @app.route('/index')
